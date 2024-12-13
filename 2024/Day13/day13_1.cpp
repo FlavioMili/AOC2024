0b23d016a5cf1f3c5fa31165067e8d77ad7652a2
Flavio Mili <flaviomilinanni@gmail.com>
2024-12-13 21:50:02 +0100

    Day13

---
+++ 2024/Day13/day13_1.cpp
@@ -0,0 +1,41 @@
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+// x1 and y1 cost 3, x2 and y2 cost 1
+// find the minimum amount of tokens
+typedef long long ll;
+
+ll calculateTokens(ll a, ll d, ll b, ll e, ll c, ll f) {
+   ll denominator = (a * e - b * d);
+   ll x = (c * e - b * f) / denominator;
+   ll y = (a * f - c * d) / denominator;
+   if(x>=0 && y>=0 && a*x+b*y == c && d*x+e*y == f)
+      return 3*x+y;
+   return 0;
+}
+
+int main() {
+   ll res = 0;
+   std::ifstream file("input.txt");
+   std::string line;
+
+   ll x1, y1, x2, y2, t1, t2;
+   while (!file.eof()) {
+      if (std::getline(file, line) && line.find("Button A") != std::string::npos) {
+         sscanf(line.c_str(), "Button A: X+%lld, Y+%lld", &x1, &y1);
+         if (std::getline(file, line) && line.find("Button B") != std::string::npos) {
+            sscanf(line.c_str(), "Button B: X+%lld, Y+%lld", &x2, &y2);
+            if (std::getline(file, line) && line.find("Prize") != std::string::npos) {
+               sscanf(line.c_str(), "Prize: X=%lld, Y=%lld", &t1, &t2);
+               ll cost = calculateTokens(x1, y1, x2, y2, t1, t2);
+               res += cost;
+            }
+         }
+      }
+   }
+   std::cout << res << "\n";
+   return 0;
+}
